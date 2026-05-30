ad6f529fa14d781d4e2c0d441e1ac7d0de379436
jainadwita <jainadwita27@gmail.com>
2026-05-30 14:05:07 +0530

    Time: 1 ms (99.92%), Space: 46.6 MB (82.12%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+        int i = 1;
+        while(i<=n){
+            if(i%3==0 && i%5==0){
+                answer.add("FizzBuzz");
+            }
+            else if(i%3 == 0){
+                answer.add("Fizz");
+            }
+            else if(i%5 == 0){
+                answer.add("Buzz");
+            }
+            else{
+                String str = String.valueOf(i);
+                answer.add(str);
+            }
+
+            i++;
+        }
+
+        return answer;
+        
+    }
+}
